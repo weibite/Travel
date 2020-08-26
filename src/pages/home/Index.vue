4991c29160c5bd13107bbae2d4031901eaa0a75a
weibite <microbit@163.com>
2020-08-26 21:58:24 +0800

    提交

---
+++ src/pages/home/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <home-header></home-header>
+    <div>首页</div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader: Header
+  }
+}
+</script>
